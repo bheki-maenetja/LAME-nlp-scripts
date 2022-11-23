0e25b4a1224bb62c9c4820a9d34f5d866c7308d3
bheki-maenetja <bhekimaenetja@gmail.com>
2022-11-23 10:54:23 +0000

    created boilerplate for simple tf-idf script

---
+++ info_extraction/tf-idf.py
@@ -0,0 +1,26 @@
+# Third-Party Imports
+from nltk import tokenize
+
+# Standard Library Imports
+import sys
+import os
+from string import punctuation
+from math import log1p
+
+# Local Imports
+from queries import get_text_cli
+
+def main():
+    pass
+
+def load_files():
+    pass
+
+def tokenize():
+    pass
+
+def compute_idfs():
+    pass
+
+def top_files():
+    pass
